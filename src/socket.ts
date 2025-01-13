4ad9ecd6f8e07bb04ee40f73000990f472f0afd1
Arnaud <arnaudadjovi274@gmail.com>
2025-01-13 04:14:50 +0100

    Update socket connection URL for production environment

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://socket-io-server-ub8o.onrender.com/';
+const URL = process.env.NODE_ENV === 'production' ? "https://socket-io-server-ub8o.onrender.com" : 'http://localhost:4000';
 
 export const socket = io(URL, {
     autoConnect: false
